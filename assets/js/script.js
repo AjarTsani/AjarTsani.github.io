6c7710e2ef394949d473663e1a48090fd5f14315
unknown <ajarbasyartsani@gmail.com>
2023-11-20 18:41:13 +0700

    update lagi

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,3 +151,13 @@ function cekTotalHarga(jenisBarang, jumlah) {
   }
 }
 showBarang();
+
+window.setTimeout('waktu()', 1000);
+
+function waktu() {
+  var waktu = new Date();
+  setTimeout('waktu()', 1000);
+  document.getElementById('jam').innerHTML = waktu.getHours();
+  document.getElementById('menit').innerHTML = waktu.getMinutes();
+  document.getElementById('detik').innerHTML = waktu.getSeconds();
+}
